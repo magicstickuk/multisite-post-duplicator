b9c196dceff5738b2a64de68a3f7e805b045b1b9
Sergio Ambel <sergiambel@gmail.com>
2015-10-10 00:36:31 +0200

    Enabled copy post feature from edit post form

--- mpd.php
+++ mpd.php
@@ -4,12 +4,13 @@
 Plugin Name: 	Multisite Post Duplicator
 Plugin URI: 	http://www.wpmaz.uk
 Description:    Duplicate any individual page, post or custom post type from one site on your multisite network to another.
-Version: 		0.3.1
-Author: 		Mario Jaconelli
+Version: 	0.4
+Author: 	Mario Jaconelli (& Sergi Ambel collaboration)
 Author URI:  	http://www.wpmaz.uk
 */
 
 
 include('inc/load-scripts.php');
+include('inc/postform_ui.php');
 include('inc/admin-ui.php');
 include('inc/core.php');
