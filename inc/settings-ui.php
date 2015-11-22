5411f28a9158dc986df9ee6b4d34d3bae7ebcbb7
Mario Jaconelli <mario.jaconelli@bipsolutions.com>
2015-11-22 11:04:34 +0000

    Fixing bug on excerpt

--- inc/settings-ui.php
+++ inc/settings-ui.php
@@ -26,9 +26,10 @@ if ( is_multisite() ) {
  */
 function mdp_add_admin_menu(  ) { 
 
-	$settingsLogic = apply_filters( 'mpd_show_settings_page', true );
+	$settingsLogic = current_user_can( 'manage_options' );
+	$settingsLogic = apply_filters( 'mpd_show_settings_page', $settingsLogic );
 
-	if(current_user_can( 'manage_options' ) && $settingsLogic ){
+	if($settingsLogic){
 		add_submenu_page( 'options-general.php', __('Multisite Post Duplicator Settings', MPD_DOMAIN ), __('Multisite Post Duplicator Settings', MPD_DOMAIN), 'manage_options', 'multisite_post_duplicator', 'mdp_options_page' );
 	}
 
--- mpd.php
+++ mpd.php
@@ -4,7 +4,7 @@
 Plugin Name: 	Multisite Post Duplicator
 Plugin URI: 	http://www.wpmaz.uk
 Description:    Duplicate any individual page, post or custom post type from one site on your multisite network to another.
-Version: 		0.6
+Version: 		0.7
 Author: 		Mario Jaconelli
 Author URI:  	http://www.wpmaz.uk
 */
