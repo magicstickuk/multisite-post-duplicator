0f257ee7f5f2c18587bc06abf27be112480873fe
Mario Jaconelli <mario.jaconelli@bipsolutions.com>
2016-11-05 17:28:34 +0000

    Reworked subdomain_install logic

--- inc/mpd-functions.php
+++ inc/mpd-functions.php
@@ -694,7 +694,7 @@ function mpd_non_multisite_admin_notice() {
         echo "<div class='error'><p>You have activated <a href='https://en-gb.wordpress.org/plugins/multisite-post-duplicator/' target='_blank'>Multisite Post Duplicator</a> on this WordPress Installation but this is not a <a target='_blank' href='http://codex.wordpress.org/Create_A_Network'>Multisite Network</a>. In the interest of your websites efficiency we would advise you deactivate the plugin until you are using a <a target='_blank' href='http://codex.wordpress.org/Create_A_Network'>Multisite Network</a></p></div>";
     }
 
-    if(is_subdomain_install() && !get_site_option('mpd_has_dismissed_subdomain_error')){
+    if(mpd_is_subdomain_install() && !get_site_option('mpd_has_dismissed_subdomain_error')){
             
             ?>
             
@@ -718,7 +718,7 @@ add_action('admin_notices', 'mpd_non_multisite_admin_notice');
  */
 function mpd_notices_javascript(){
 
-    if(is_subdomain_install()){
+    if(mpd_is_subdomain_install()){
     ?>
     <script>
         jQuery(document).on('ready', function() {
@@ -1087,5 +1087,20 @@ function mpd_do_settings_link(){
 
 }
 add_action('mpd_after_metabox_content', 'mpd_do_settings_link');
+
+function mpd_is_subdomain_install(){
+
+    if(function_exists('is_subdomain_install')){
+        
+        return is_subdomain_install();
+        
+    }else{
+
+        return false;
+
+    }
+
+
+}
 
 
