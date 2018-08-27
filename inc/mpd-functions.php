428631f06e50ecb3c562995fb8bbd2a02190b571
Jonathan Moore <Jonathan.Moore@bcs.org>
2018-08-27 09:55:23 +0100

    correction to post media defensive code fix

--- inc/mpd-functions.php
+++ inc/mpd-functions.php
@@ -646,7 +646,7 @@ function mpd_get_image_alt_tags($post_media_attachments){
               $post_id = $post_media_attachment->ID;
             }
 			      if ( $post_id ) {
-                $alt_tag = get_post_meta($post_media_attachment->ID, '_wp_attachment_image_alt', true);
+				$alt_tag = get_post_meta( $post_id, '_wp_attachment_image_alt', true );
 
                 $alt_tags_to_be_copied[$attachement_count] = $alt_tag;
 
